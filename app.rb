df00e1d080d86b2e108700c92e03cb03665a2879
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-11 14:43:06 +0300

    commit tree e0bbe698b88c3737470c1abaf6e69aa8a4c3364e

---
+++ app.rb
@@ -0,0 +1,34 @@
+# app.rb
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+
+
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# p "Hello world!"
+# p "Hello world!"
+# p "Hello world!"
+# p "Hello world!"
+
+# pp "Hello world!" //pretty printing
+
+# puts [1,2,3]
+# print [1,2,3]
+# p [1,2,3]  //Equivalent to puts [1,2,3].inspect
+# pp [1,2,3] //.pretty_inspect method
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# p [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# print [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+
